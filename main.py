b48655576976e627fcea6341cfb876548be5da6b
MuslicaCrew <360bruno360@gmail.com>
2025-04-29 22:07:51 +0200

    Added masking feature

--- main.py
+++ main.py
@@ -1,64 +1,52 @@
 # This file is only used to imlement clache on the input file.
 import numpy as np
-import imageio
 import matplotlib.pyplot as plt
 import pydicom as dicom
-import cv2
-from skimage.restoration import denoise_tv_chambolle
 import os
 from PIL import Image
+import nibabel as nib
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from utils import modify_image, plot, hu_units
 #%%
 file = "D:\\Szakdoga\\SortedPictures\\3\\1-15.dcm"
 
+# Desktop/OneDrive_1_17-02-2024/week_4/het4_morfologia_orai.ipynb
+
+dd = dicom.dcmread(file)
+
+image = modify_image(file)
+
+# image = (dd.pixel_array)/16
+
+print(image[100,100])
+print(image.shape)
+
+plt.subplot(1, 2, 1)
+plt.imshow(image, cmap='gray')
+plt.axis('on')
+
+
+plt.subplot(1, 2, 2)
+plt.imshow(image[150:250,75:175], cmap='gray')
+plt.axis('on')
 #%%
-def modify_image(file):
-   dicom_data = dicom.dcmread(file)
-   image_data = dicom_data.pixel_array/16
-   image_data -= np.min(image_data)
-   image_new = image_data / np.max(image_data)
-   image_new *= 255
-   image_uint8 = image_new.astype(np.uint8) 
-   claheObj = cv2.createCLAHE(clipLimit=5)
-   claheImage = claheObj.apply(image_uint8)
-   denoised = cv2.fastNlMeansDenoising(claheImage, None, h=4, templateWindowSize=7, searchWindowSize=21)
-   claheImage_denoised = claheObj.apply(denoised)
-   return claheImage_denoised
+path = 'D:\\Szakdoga\\LCI\\100046.nii'
+
+img = nib.load(path).get_fdata()
+# img.shape
+img2 = img[:,:,52] < 0
+plt.subplot(1, 2, 2)
+plt.imshow(img2, cmap='gray')
+plt.axis('on')
 #%%
-# dicom_data = dicom.dcmread(file)
-# image_data = dicom_data.pixel_array/16
-# image_data -= np.min(image_data)
-# image_new = image_data / np.max(image_data)
-# image_new *= 255
-# image_uint8 = image_new.astype(np.uint8)
-
-# claheObj = cv2.createCLAHE(clipLimit=5)
-# claheImage = claheObj.apply(image_uint8)
-# denoised = cv2.fastNlMeansDenoising(image_uint8, None, h=4, templateWindowSize=7, searchWindowSize=21)
-# img_float = claheImage / 255.0
-# denoised_tv = denoise_tv_chambolle(img_float, weight=0.01)
-# denoised_uint8 = (denoised_tv * 255).astype(np.uint8)
-# claheImage_denoised = claheObj.apply(denoised)
-
-# plt.figure(figsize=(12, 4))
-# plt.subplot(1, 3, 1)
-# plt.title("Original")
-# plt.imshow(image_uint8, cmap='gray')
-# plt.axis('off')
-
-# plt.subplot(1, 2, 1)
-# plt.title("After CLAHE")
-# plt.imshow(claheImage, cmap='gray')
-# plt.axis('off')
-
-# plt.subplot(1, 2, 2)
-# plt.title("After NLMD")
-# plt.imshow(denoised, cmap='gray')
-# plt.axis('off')
-
-# plt.subplot(1, 2, 2)
-# plt.title("After tv_chambolle")
-# plt.imshow(denoised_uint8, cmap='gray')
-# plt.axis('off')
+plt.figure(figsize=(12, 4))
+plt.subplot(1, 2, 1)
+plt.imshow(image, cmap='gray')
+plt.axis('on')
+plt.subplot(1, 2, 2)
+plt.imshow(image, cmap='gray')
+plt.axis('on')
 
 """
 noise cancellation before clahe seems to produce better visibility
@@ -71,7 +59,7 @@ h = 4 works best from 5 it is too blurry
 # plt.axis('off')
 
 
-# plt.figure(figsize=(12, 4))
+# 
 # plt.subplot(1, 2, 1)
 # plt.title("image")
 # plt.imshow(image, cmap='gray')
@@ -85,24 +73,28 @@ h = 4 works best from 5 it is too blurry
 
 
 # plt.show()
+
+#%%
+np.dtype(modify_image(file_path)[0,0])
 #%%
 directory = "D:\\Szakdoga\\SortedPictures\\3\\"
 images = []
 for filename in os.listdir(directory):
     file_path = os.path.join(directory, filename)
     if os.path.isfile(file_path):
-       images.append(modify_image(file_path))
-        
+        masked = modify_image(file_path) & (hu_units(file_path) * 255)
+        images.append(masked)
 
+plot(images[1], "test2")
 #%% Making images all uniform uint8 and RGB
 
 processed_images = []
 for img in images:
-    # Convert float images to 0-255 uint8
-    if img.dtype in [np.float32, np.float64]:
-        img = (img * 255).clip(0, 255).astype(np.uint8)
-    elif img.dtype != np.uint8:
-        img = img.astype(np.uint8)
+    # # Convert float images to 0-255 uint8
+    # if img.dtype in [np.float32, np.float64]:
+    #     img = (img * 255).clip(0, 255).astype(np.uint8)
+    # elif img.dtype != np.uint8:
+    #     img = img.astype(np.uint8)
 
     # Convert grayscale to RGB for compatibility
     if len(img.shape) == 2:
@@ -113,7 +105,7 @@ for img in images:
     processed_images.append(img)
 #%% Save to GIF
 # Convert NumPy arrays to PIL images
-pil_images = [Image.fromarray(img) for img in processed_images]
+pil_images = [Image.fromarray(img) for img in images]
 
 # Save with duration per frame (ms), loop=0 means infinite
 pil_images[0].save(
